8935724cedc39d48b0760df375174be2ca663bc6
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-09 11:31:54 -0500

    springboot lecture example

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,21 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+//    @GetMapping("/hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello from Spring Boot to my friends in Marco!";
+//    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name){
+        return "Hello from Spring Boot to " + name +" in Marco!";
+    }
+}
