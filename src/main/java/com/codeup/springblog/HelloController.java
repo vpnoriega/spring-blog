d8187beb04848f05a13f9cc42edb9efcd33d8a23
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-11 09:32:19 -0500

    lecture and set up

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,21 +1,29 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-//    @GetMapping("/hello")
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello from Spring Boot to my friends in Marco!";
+    }
+
+//    @GetMapping("/hello/{name}")
 //    @ResponseBody
-//    public String hello(){
-//        return "Hello from Spring Boot to my friends in Marco!";
+//    public String helloName(@PathVariable String name){
+//        return "Hello from Spring Boot to " + name +" in Marco!";
 //    }
 
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String helloName(@PathVariable String name){
-        return "Hello from Spring Boot to " + name +" in Marco!";
+    public String hello(@PathVariable String name,
+    Model model){
+        model.addAttribute("UsersName", name);
+        return "helloUser";
     }
 }
---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,24 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String diceGame(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String play(@PathVariable int guess, Model model){
+        int random = (int)(Math.random() * 6 + 1);
+    }
+
+    //model.addAttribute("result", result)
+}
