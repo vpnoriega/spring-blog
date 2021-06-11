fb68d828117a561075fc5e8ac32a55c2fcd9a774
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-11 14:50:43 -0500

    In progress: views exercise

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,4 +1,4 @@
-package com.codeup.springblog;
+//package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Random;
 
-@Controller
-public class RollDiceController {
-
-    @GetMapping("/roll-dice")
-    public String diceGame(){
-        return "roll-dice";
-    }
-
-    @GetMapping("/roll-dice/{guess}")
-    public String play(@PathVariable int guess, Model model){
-        int random = (int)(Math.random() * 6 + 1);
-    }
+//@Controller
+//public class RollDiceController {
+//
+//    @GetMapping("/roll-dice")
+//    public String diceGame(){
+//        return "roll-dice";
+//    }
+//
+//    @GetMapping("/roll-dice/{guess}")
+//    public String play(@PathVariable int guess, Model model){
+//        int random = (int)(Math.random() * 6 + 1);
+//    }
 
     //model.addAttribute("result", result)
-}
+//}
