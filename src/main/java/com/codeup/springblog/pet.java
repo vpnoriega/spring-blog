6a85a21064a17d87329ae5fbf2e8f4578d2b1e1a
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-11 14:50:09 -0500

    Pets Workshop

---
+++ src/main/java/com/codeup/springblog/pet.java
@@ -0,0 +1,20 @@
+package com.codeup.springblog;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pets")
+public class pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String species;
+}
