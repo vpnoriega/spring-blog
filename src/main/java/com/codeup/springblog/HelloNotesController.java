a531385037548dc77a2e8ea1f9f3b3e9afbb5a60
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-12 14:32:09 -0500

    Spring Notes: Controllers

---
+++ src/main/java/com/codeup/springblog/HelloNotesController.java
@@ -0,0 +1,29 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller //defines the class of the controller
+public class HelloNotesController {
+    @GetMapping("/hellonotes") // defines the method that should be invoked when a GET request is received from the URI
+    @ResponseBody //tells spring that whatever is returned should be in the BODY of our reponse
+    public String hello(){
+        return "Hello there from my notes!";
+    }
+
+    //path variables are part of the URI or a request as opposed to being passed as a query string
+    @GetMapping("/hellonotes/{fruit}")
+    @ResponseBody
+    public String sayHello(@PathVariable String fruit){
+        return "There are 5 " + fruit + " left in the fruit basket";
+    }
+
+    //Defining the parameter with a different type.
+    //@RequestMapping annotation is the longer version of @GetMapping
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
+
